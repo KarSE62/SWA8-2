c3a14946d805d5be1079e827ed46c1c1ea9339f6
KarSE62 <59428783+KarSE62@users.noreply.github.com>
2021-08-23 15:24:07 +0700

    Initial commit

---
+++ src/Cilent.java
@@ -0,0 +1,10 @@
+
+public class Cilent {
+
+	public static void main(String[] args) {
+		IUser iuser = new EmployeeUser();
+		UserManager user = new UserManager(iuser);
+		System.out.println(" " + user.getName());
+	}
+
+}
